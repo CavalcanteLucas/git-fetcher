bb6fdb28162a50d1e4f9daa960f78cc6b6b3e8a3
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2022-07-10 10:58:14 -0300

    Add pagination support to CommitList

--- src/CommitList.js
+++ src/CommitList.js
@@ -7,20 +7,50 @@ export default function CommitList({ branchname, username, reponame }) {
   const [isLoadingCommits, setIsLoadingCommits] = useState(false);
   const [errCommits, setErrCommits] = useState("");
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+
   const getCommits = () => {
     setCommits([]);
     setIsLoadingCommits(true);
     setErrCommits("");
     axios
-      .get(`https://api.github.com/repos/${username}/${reponame}/commits\?sha=${branchname}`, {
-        headers: {
-          Authorization: `token ${process.env.REACT_APP_GIT_TOKEN}`,
-        },
-      })
+      .get(
+        `https://api.github.com/repos/${username}/${reponame}/commits?sha=${branchname}&per_page=10&page=${currentPage}`,
+        {
+          headers: {
+            Authorization: `token ${process.env.REACT_APP_GIT_TOKEN}`,
+          },
+        }
+      )
       .then((response) => {
         console.log(response.data);
         setCommits(response.data);
         setErrCommits("");
+
+        const headerLink = response.headers.link;
+
+        if (headerLink) {
+          const paginationLinks = headerLink.split(", ");
+
+          paginationLinks.map((paginationLink) => {
+            console.log(paginationLink);
+
+            const paginationURL = paginationLink
+              .split("; ")[0]
+              .match("<(.*)>")[1];
+            const paginationRel = paginationLink
+              .split("; ")[1]
+              .match('"(.*)"')[1];
+
+            if (paginationRel === "last") {
+              const lastPage = paginationURL.match(/&page=(\d+).*$/)[1];
+              setLastPage(parseInt(lastPage));
+            }
+
+            return null;
+          });
+        }
       })
       .catch((error) => {
         setErrCommits(error.message);
@@ -30,7 +60,7 @@ export default function CommitList({ branchname, username, reponame }) {
       });
   };
 
-  useEffect(getCommits, [branchname, reponame, username]);
+  useEffect(getCommits, [branchname, currentPage, reponame, username]);
 
   if (!commits) return null;
 
@@ -39,7 +69,15 @@ export default function CommitList({ branchname, username, reponame }) {
       <div className="commit-list-frame">
         {errCommits && <h4>{errCommits}</h4>}
         {isLoadingCommits && <h4>Loading...</h4>}
-
+        {
+          <div className="commit-list-pg-btn-frame">
+            <button>⬅</button>
+            <h3>
+              {currentPage} | {lastPage}
+            </h3>
+            <button>➡</button>
+          </div>
+        }
         {commits.map((commit) => {
           return <Commit key={commit.sha} commit={commit}></Commit>;
         })}
